6f9c3cf0e9f77062302b549703fea1083ad99eb9
ktat <ktat.is@gmail.com>
2012-06-26 02:57:12 +0900

    fix typo. move PLACK_ENV place

--- script/update_deployment.sh
+++ script/update_deployment.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-PLACK_ENV=deployement
-
 cd /var/lib/jpa/perldoc.jp/code;
 PERL='/var/lib/jpa/perl5/perls/perl-5.14.2/bin/perl -Mlib=./extlib/lib/perl5 -Ilib'
-$PERL script/update.pl
-
+PLACK_ENV=deployment $PERL script/update.pl
