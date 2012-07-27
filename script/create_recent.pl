a3cee369318ac3d065d70113bb7758b8560bdb30
ktat <ktat.is@gmail.com>
2012-07-27 09:39:35 +0900

    untabify. fix rss.

--- script/create_recent.pl
+++ script/create_recent.pl
@@ -21,12 +21,12 @@ main();
 sub main {
     my $updates = create_recent_data();
     if (create_file($updates)) {
-	create_rss($updates);
+        create_rss($updates);
     }
 }
 
 sub create_recent_data {
-    my $pjp	   = PJP->bootstrap;
+    my $pjp        = PJP->bootstrap;
     my $config     = $pjp->config;
     my $mode_name  = $pjp->mode_name || 'development';
 
@@ -40,25 +40,25 @@ sub create_recent_data {
     my @updates;
 
     1 while $cvs =~ s{^. (\d{4}-\d{2}-\d{2}) \d{2}:\d{2} \+0000 ([^ ]+) +[\d\.]+ +([^ ]+) +([^ ]+)}{
-	push @updates, {
-	    date   => $1,
-	    author => $2,
-	    path   => "$4/$3",
-	    name   => file2name("$4/$3"),
-	}
+        push @updates, {
+            date   => $1,
+            author => $2,
+            path   => "$4/$3",
+            name   => file2name("$4/$3"),
+        }
     }em;
 
     foreach my $repos (qw/Moose-Doc-JA MooseX-Getopt-Doc-JA/) {
-	# Mon Oct 26 12:04:50 2009 -- BUILDARGS-DEMOLISH -- BUILDARGS, DEMOLISH -- lestrrat
-	#
-	# Moose/Manual/Construction.pod
-	foreach my $file (File::Find::Rule->file()->name('*.pod')->in("$assets_dir$repos")) {
-	    my $git = qx{cd $assets_dir/$repos/; git log -1 --date=iso --pretty='%cd -- %an' --since='$date'} or next;
-	    my ($date, $author) = $git =~m{^(\d{4}-\d{2}-\d{2}) \d{2}:\d{2}:\d{2} \+\d{4} -- (.+)$} or die $git;
-	    $file =~s{^.+?assets/}{};
-	    $file =~s{^\Q$repos/\E}{};
-	    push @updates, {date => $date, author => $author, path => 'docs/modules/' . $file, name => file2name($file)};
-	}
+        # Mon Oct 26 12:04:50 2009 -- BUILDARGS-DEMOLISH -- BUILDARGS, DEMOLISH -- lestrrat
+        #
+        # Moose/Manual/Construction.pod
+        foreach my $file (File::Find::Rule->file()->name('*.pod')->in("$assets_dir$repos")) {
+            my $git = qx{cd $assets_dir/$repos/; git log -1 --date=iso --pretty='%cd -- %an' --since='$date'} or next;
+            my ($date, $author) = $git =~m{^(\d{4}-\d{2}-\d{2}) \d{2}:\d{2}:\d{2} \+\d{4} -- (.+)$} or die $git;
+            $file =~s{^.+?assets/}{};
+            $file =~s{^\Q$repos/\E}{};
+            push @updates, {date => $date, author => $author, path => 'docs/modules/' . $file, name => file2name($file)};
+        }
     }
     return \@updates;
 }
@@ -73,11 +73,11 @@ sub create_file {
     print $fh Dumper([( sort {($tmp{$b} ||= $b->{date}) cmp ($tmp{$a} ||= $a->{date})} @$updates )[0 .. 50]]);
     close $fh;
     if (! -e "data/recent.pl" or qx{diff data/recent.pl data/recent.pl.new}) {
-	rename "data/recent.pl.new", "data/recent.pl";
-	return 1;
+        rename "data/recent.pl.new", "data/recent.pl";
+        return 1;
     } else {
-	unlink "data/recent.pl.new";
-	return 0;
+        unlink "data/recent.pl.new";
+        return 0;
     }
 }
 
@@ -87,28 +87,30 @@ sub create_rss {
     mkdir 'static/rss' or die $! if not -d 'static/rss';
 
     my $mtime = (stat "data/recent.pl")[9];
-    my $date = (localtime $mtime)->strftime("%a, %d %b %Y %H:%M:%S +0900");
+    my $datetime = (localtime $mtime)->strftime("%a, %d %b %Y %H:%M:%S +0900");
 
     my $rss = XML::RSS->new(version => '2.0');
     $rss->channel(
-	title          => 'perldoc.jp',
-	link           => 'http://www.perldoc.jp/',
-	language       => 'ja',
-	description    => 'Perl の公式ドキュメント、モジュールを日本語翻訳したものを表示するサイトです。',
-	copyright      => 'Japan Perl Association',
-	pubDate        => $date,
-	lastBuildDate  => $date,
-	managingEditor => 'ktat@perlassociations.jp',
-	webMaster      => 'ktat@perlassociations.jp',
-	);
+        title          => 'perldoc.jp',
+        link           => 'http://perldoc.jp/',
+        language       => 'ja',
+        description    => 'Perl の公式ドキュメント、モジュールを日本語翻訳したものを表示するサイトです。',
+        copyright      => 'Japan Perl Association',
+        pubDate        => $datetime,
+        lastBuildDate  => $datetime,
+        managingEditor => 'ktat@perlassociations.jp',
+        webMaster      => 'ktat@perlassociations.jp',
+        );
 
     my $recent = do "data/recent.pl";
     foreach my $module (@{$recent}[0 .. 50]) {
-	$rss->add_item(
-	    title       => $module->{name},
-	    permaLink   => "http://www.perldoc.jp" . $module->{path},
-	    description => "$module->{name} が、$module->{author} により commit されました。",
-	    );
+        my $datetime = Time::Piece->strptime($module->{date}, '%Y-%m-%d %H:%M:%S');
+        $rss->add_item(
+            title       => $module->{name},
+            link        => "http://www.perldoc.jp" . $module->{path},
+            description => "$module->{name} が、$module->{author} により commit されました。",
+            pubDate     => $datetime->strftime("%a, %d %b %Y %H:%M:%S +0900"),
+            );
     }
     open my $fh, '>', 'static/rss/recent.rss' or die $!;
     print $fh $rss->as_string;
