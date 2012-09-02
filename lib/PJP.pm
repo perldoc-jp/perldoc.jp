6255b6e38424e8d095cca8f24483c0158fea4380
ktat <ktat.is@gmail.com>
2012-09-02 13:21:59 +0900

    move   abstract_title_description method

--- lib/PJP.pm
+++ lib/PJP.pm
@@ -14,14 +14,29 @@ sub cache { $cache }
 
 use PJP::DBI;
 sub dbh {
-	my $c = shift;
-	my $conf = $c->config->{DB} // die "Missing mandatory configuraion parameter: DB";
-	return $c->{db} //= PJP::DBI->connect(@$conf);
+        my $c = shift;
+        my $conf = $c->config->{DB} // die "Missing mandatory configuraion parameter: DB";
+        return $c->{db} //= PJP::DBI->connect(@$conf);
 }
 
 sub assets_dir {
     my $c = shift;
     $c->config->{assets_dir} // die "Missing configuration for assets dir";
+}
+
+sub abstract_title_description {
+  my ($c, $html) = @_;
+  my $title;
+  if ($html =~ m{<title>(.*?)</title>}) {
+    $title = $1;
+  } elsif ($html =~ m{<h1>(.*?)</h1>}) {
+    $title = $1;
+  }
+  my $abstract;
+  if ($html =~ m{<meta\s+name="description"\s+content="([^"]+)">}i) {
+    $abstract = $1;
+  }
+  return ($title, $abstract)
 }
 
 1;
