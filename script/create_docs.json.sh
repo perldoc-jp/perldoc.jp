619f415721b8d013a0b5a2381e4846c0b8c32c3f
ktat <ktat.is@gmail.com>
2013-03-20 19:35:46 +0900

    fix script/create_docs.json.sh. path was wrong.

--- script/create_docs.json.sh
+++ script/create_docs.json.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sqlite3 db/perldocjp.db  'select package,path from pod' | perl -e 'use JSON::XS qw/encode_json/; my %d; while(<>) {chomp; my($m, $p) = split "\\|", $_, 2; $d{$m} = $p} print encode_json(\%d)' > /tmp/perldoc.jp.docs.json
-mv /tmp/perldoc.jp.docs.json /var/lib/jpa/perldocjp/code/static/docs.json
+sqlite3 ../db/perldocjp.db  'select package,path from pod' | perl -e 'use JSON::XS qw/encode_json/; my %d; while(<>) {chomp; my($m, $p) = split "\\|", $_, 2; $d{$m} = $p} print encode_json(\%d)' > /tmp/perldoc.jp.docs.json
+mv /tmp/perldoc.jp.docs.json /var/lib/jpa/perldoc.jp/code/static/docs.json
