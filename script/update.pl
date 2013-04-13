8b19bc9378d4be028eee6fd0eae94dbf691209a6
ktat <ktat.is@gmail.com>
2013-04-13 20:02:18 +0900

    uncoment lines which only comment for test.

--- script/update.pl
+++ script/update.pl
@@ -20,32 +20,32 @@ if (! -d $assets_dir) {
     mkdir $assets_dir;
 }
 
-# if (! -d $assets_dir . '/perldoc.jp/') {
-# 
-#     mkdir $assets_dir . '/perldoc.jp/';
-#     system(<<_SHELL_);
-# cd $assets_dir/perldoc.jp;
-# cvs -d:pserver:anonymous\@cvs.sourceforge.jp:/cvsroot/perldocjp login;
-# cvs -z3 -d:pserver:anonymous\@cvs.sourceforge.jp:/cvsroot/perldocjp co docs;
-# _SHELL_
-# 
-# } else {
-#     system(qq{cd $assets_dir/perldoc.jp/docs/; cvs up -dP});
-# }
-# 
-# if (! -d $assets_dir . '/module-pod-jp/') {
-#     system(qq{git clone git://github.com/perldoc-jp/module-pod-jp.git $assets_dir/module-pod-jp/});
-# }
-# 
-# system(qq{cd $assets_dir/module-pod-jp; git pull origin master});
-# 
-# foreach my $jpa_module (qw/MooseX-Getopt-Doc-JA  Moose-Doc-JA/) {
-#     if (! -d $assets_dir . "/$jpa_module/") {
-# 	system(qq{cd $assets_dir; git clone git://github.com/jpa/$jpa_module.git});
-#     }
-# 
-#     system(qq{cd $assets_dir/$jpa_module; git pull origin master});
-# }
+if (! -d $assets_dir . '/perldoc.jp/') {
+
+    mkdir $assets_dir . '/perldoc.jp/';
+    system(<<_SHELL_);
+cd $assets_dir/perldoc.jp;
+cvs -d:pserver:anonymous\@cvs.sourceforge.jp:/cvsroot/perldocjp login;
+cvs -z3 -d:pserver:anonymous\@cvs.sourceforge.jp:/cvsroot/perldocjp co docs;
+_SHELL_
+
+} else {
+    system(qq{cd $assets_dir/perldoc.jp/docs/; cvs up -dP});
+}
+
+if (! -d $assets_dir . '/module-pod-jp/') {
+    system(qq{git clone git://github.com/perldoc-jp/module-pod-jp.git $assets_dir/module-pod-jp/});
+}
+
+system(qq{cd $assets_dir/module-pod-jp; git pull origin master});
+
+foreach my $jpa_module (qw/MooseX-Getopt-Doc-JA  Moose-Doc-JA/) {
+    if (! -d $assets_dir . "/$jpa_module/") {
+	system(qq{cd $assets_dir; git clone git://github.com/jpa/$jpa_module.git});
+    }
+
+    system(qq{cd $assets_dir/$jpa_module; git pull origin master});
+}
 
 unlink "$assets_dir/index-module.pl";
 unlink "$assets_dir/index-article.pl";
@@ -56,11 +56,11 @@ if (! -e $sqlite_db) {
 }
 
 my $t = time;
-#PJP::M::Index::Article->generate_and_save($pjp);
-#PJP::M::Index::Module->generate_and_save($pjp);
+PJP::M::Index::Article->generate_and_save($pjp);
+PJP::M::Index::Module->generate_and_save($pjp);
 PJP::M::BuiltinFunction->generate($pjp);
-#PJP::M::BuiltinVariable->generate($pjp);
-#PJP::M::PodFile->generate($pjp);
+PJP::M::BuiltinVariable->generate($pjp);
+PJP::M::PodFile->generate($pjp);
 
 if ($config->{master_db} and -e $config->{master_db} and $config->{slave_db}) {
   system('cp ' . $config->{master_db} . ' ' . $config->{slave_db});
