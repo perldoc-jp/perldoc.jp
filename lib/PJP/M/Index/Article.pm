01293c9903c333021c122cd1769e59111a2bf57e
ktat <ktat.is@gmail.com>
2013-05-03 17:53:04 +0900

    order by ctime of file desc

--- lib/PJP/M/Index/Article.pm
+++ lib/PJP/M/Index/Article.pm
@@ -57,13 +57,15 @@ sub generate {
     my ($class, $c) = @_;
 
     # 情報をかきあつめる
-    my @articles;
+    my @files;
     for my $base (map { File::Spec->catdir( $c->assets_dir(), $_) } qw(
         perldoc.jp/docs/articles/
         module-pod-jp/docs/articles/
     )) {
-        push @articles, $class->_generate($c, $base) if -d $base;
+        push @files, $class->_get_files($c, $base) if -d $base;
     }
+
+    my @articles = $class->_generate($c, \@files);
 
     return  map {
         +{
@@ -74,10 +76,10 @@ sub generate {
           latest_version => 0,
           versions => [],
          }
-    } @articles
+    } @articles;
 }
 
-sub _generate {
+sub _get_files {
     my ($class, $c, $base) = @_;
 
     my $repository = do {
@@ -87,8 +89,8 @@ sub _generate {
         $_;
     };
 
-    my @mods;
     opendir(my $dh, $base);
+    my @all_files;
     while (defined(my $e = readdir $dh)) {
         next if $e =~ /^\./;
         next if $e =~ /^CVS$/;
@@ -96,48 +98,55 @@ sub _generate {
         my (@files) = File::Find::Rule->file()
             ->name(qr/\.(pod|html|md)$/)
             ->in("$base/$e");
+        push @all_files, @files;
+    }
+    map {[$repository, $_]} @all_files;
+}
 
+sub _generate {
+    my ($class, $c, $files) = @_;
+
+    my @mods;
+    foreach my $repo_file (sort {-M $a->[1] <=> -M $b->[1]} @$files) {
+        my ($repository, $file) = @$repo_file;
         my $is_pod;
-        foreach my $file (@files) {
-            my ($row, $package, $dist, $distvname, $abstract);
-            $distvname = $file;
-            $distvname =~ s{^.*?articles/}{};
-            if ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+)\.html$}) {
-                $is_pod = 0;
-                ($package, $dist) = ($1, $2);
-                ($dist, $abstract) = $c->abstract_title_description(scalar slurp($file));
-            } elsif ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+)\.md$}) {
-                $is_pod  = 0;
-                ($package, $dist) = ($1, $2);
-                ($dist, $abstract) = $c->abstract_title_description_from_md(scalar slurp($file));
-            } elsif ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+?)\.pod$}) {
-                $is_pod  = 1;
-                ($package, $dist) = ($1, $2);
-            }
-            $row = {
-                    name       => $dist,
-                    version    => 0,
-                    package    => $package,
-                    distvname  => $distvname,
-                    repository => $repository,
-                    abstract   => $abstract,
-                   };
-
-            if ($is_pod) {
-                infof("parsing %s", $file);
-                my ($name, $desc) = PJP::M::Pod->parse_name_section($file);
-                if ($desc) {
-                    infof("Japanese Description: %s, %s", $name, $desc);
-                    $row->{abstract} = $desc;
-                }
-            }
-
-            push @mods, $row;
+        my ($row, $package, $dist, $distvname, $abstract);
+        $distvname = $file;
+        $distvname =~ s{^.*?articles/}{};
+        if ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+)\.html$}) {
+            $is_pod = 0;
+            ($package, $dist) = ($1, $2);
+            ($dist, $abstract) = $c->abstract_title_description(scalar slurp($file));
+        } elsif ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+)\.md$}) {
+            $is_pod  = 0;
+            ($package, $dist) = ($1, $2);
+            ($dist, $abstract) = $c->abstract_title_description_from_md(scalar slurp($file));
+        } elsif ($file =~ m{^.*?articles/([^/]+)/(?:.*?/)?([^/]+?)\.pod$}) {
+            $is_pod  = 1;
+            ($package, $dist) = ($1, $2);
         }
+        $row = {
+                name       => $dist,
+                version    => 0,
+                package    => $package,
+                distvname  => $distvname,
+                repository => $repository,
+                abstract   => $abstract,
+               };
+
+        if ($is_pod) {
+            infof("parsing %s", $file);
+            my ($name, $desc) = PJP::M::Pod->parse_name_section($file);
+            if ($desc) {
+                infof("Japanese Description: %s, %s", $name, $desc);
+                $row->{abstract} = $desc;
+            }
+        }
+
+        push @mods, $row;
     }
 
     return @mods;
 }
 
 1;
-
