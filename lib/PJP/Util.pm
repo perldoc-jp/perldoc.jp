bbfa8091d283fcc17413cece1f4835e0c5ad5cc6
kobaken <kentafly88@gmail.com>
2024-04-25 21:33:00 +0900

    markdownをhtmlに変換するユーティリティ追加

--- lib/PJP/Util.pm
+++ lib/PJP/Util.pm
@@ -2,9 +2,12 @@ package PJP::Util;
 
 use strict;
 use warnings;
+use feature qw(state);
 use parent 'Exporter';
 
-our @EXPORT_OK = qw/slurp/;
+use Markdent::Simple::Document;
+
+our @EXPORT_OK = qw/slurp markdown_to_html/;
 
 sub slurp {
     if (@_==1) {
@@ -14,6 +17,25 @@ sub slurp {
     } else {
         die "not implemented yet.";
     }
+}
+
+sub markdown_to_html {
+    my ($markdown, $title) = @_;
+    $title //= 'PJP';
+
+    state $parser = Markdent::Simple::Document->new;
+    my $html = $parser->markdown_to_html(
+        title    => $title,
+        dialect  => 'GitHub',
+        markdown => $markdown,
+    );
+
+    # perldoc.jp 用の加工
+    $html =~ s{^.*<(?:body)[^>]*>}{}si;
+    $html =~ s{</(?:body)>.*$}{}si;
+    $html =~ s{<!--\s+original(.*?)-->}{<div class="original">$1</div>}sg;
+
+    return $html;
 }
 
 1;
